9fe06c7a690bbae6d9aa1571383f9cbd3ee72ae2
小野 純平 <snorlax.chemist.and.jazz@gmail.com>
2019-07-02 15:56:30 +0900

    Initial Commit

---
+++ ayame-ios-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ayame-ios-sample
+//
+//  Created by 小野 純平 on 2019/07/02.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
